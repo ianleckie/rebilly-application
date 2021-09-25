fa513d64c1eafbbf295be82520360050a22570d1
ianleckie <ianleckie@hey.com>
2021-09-24 23:42:15 -0500

    Update test-server.js

--- test-server.js
+++ test-server.js
@@ -1,32 +1,36 @@
 // A quick and dirty REST API server for ApplyByAPI testing
 
+// modules
 const express    = require( 'express' );
 const parser     = require( 'body-parser' );
 const formidable = require( 'formidable' );
 const path       = require( 'path' );
 const fs         = require( 'fs' );
 
+// variables
+const successToken    = 'SUCCESS-TOKEN';
+const badToken        = 'Bad token';
+const missingToken    = 'Missing token';
+const applicationsDir = 'applications';
+
 const app = express();
 
-const successToken = 'SUCCESS-TOKEN';
-
-app.use( parser.json() ).post( '/gentoken', function ( request, response ) {
+app.use( parser.json() ).post( '/gentoken', ( request, response ) => {
 
 	let output = {};
 
 	// if posting value is valid output the success token
-	// otherwise, output the appropriate error
 	if ( request.body.posting === 7 ) {
 
 		output = { token: successToken };
 	
 	} else if ( request.body.posting ) {
 		
-		output = { error: 'bad token' };
+		output = { error: badToken };
 	
 	} else {
 		
-		output = { error: 'missing token' };
+		output = { error: missingToken };
 	
 	}
 
@@ -34,64 +38,54 @@ app.use( parser.json() ).post( '/gentoken', function ( request, response ) {
 
 });
 
-app.post( '/apply', function ( request, response, next ) {
-
-	const form = formidable({ multiples: true });
+app.post( '/apply', ( request, response, next ) => {
 
 	let output = {};
-
-	let applicationId =Math.floor( ( Math.random() * 100000 ) + 1 );
+	
+	const form = formidable({ multiples: true });
+	
+	const applicationId = Math.floor( ( Math.random() * 100000 ) + 1 );
 
 	form.parse( request, ( formError, fields, files ) => {
 		
 		if ( formError ) {
 			
 			next( formError );
-			return;
+			return;		
 		
 		}
 
-		// if the token value is valid save the request data and output a random
-		// application id. otherwise, output the appropriate error
+		// if the token value is valid save the request data and output a random application id
 		if ( fields.token === successToken) {
 
 			// add the application ID to the field data
 			fields.applicationId = applicationId;
 			
-			let fieldsJSON = JSON.stringify( fields, null, 4 );
-
 			// save request fields as a JSON file in the applications directory
-			fs.writeFileSync( path.join( process.cwd(), 'applications', applicationId + '-' + fields.email + '.json' ), fieldsJSON, ( jsonError ) => {
+			const fieldsJSON = JSON.stringify( fields, null, 4 );
+
+			fs.writeFileSync( path.join( process.cwd(), applicationsDir, applicationId + '-' + fields.email + '.json' ), fieldsJSON, ( jsonError ) => {
 				
-				if ( jsonError ) {
-					
-					throw jsonError;
-				
-				}
+				if ( jsonError ) { throw jsonError }			
 			
 			});
 
 			// move the uploaded resume file to the applications directory
-			fs.rename( files.resume.path, path.join( process.cwd(), 'applications', applicationId + '-' + fields.email + '-' + files.resume.name ), ( fileError ) => {
+			fs.rename( files.resume.path, path.join( process.cwd(), applicationsDir, applicationId + '-' + fields.email + '-' + files.resume.name ), ( fileError ) => {				
 				
-				if ( fileError ) {
-					
-					throw fileError
-				
-				}
+				if ( fileError ) { throw fileError }
 			
 			});
 
-			// output random application id
 			output = { applicationID: applicationId };
 		
 		} else if ( fields.token ) {
 		
-			output = { error: 'bad token' };
+			output = { error: badToken };
 	
 		} else {
 			
-			output = { error: 'missing token' };
+			output = { error: missingToken };
 		
 		}
 
